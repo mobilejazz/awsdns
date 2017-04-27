ead9f13b804a1eac7ebde541210da96f4ebb4996
Jordi Giménez Gámez <jordi@mobilejazz.com>
2017-04-27 21:21:29 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,92 @@
+# awsdns - DNS naming for your EC2 instances
+
+`awsdns` is a DNS server that lets you discover EC2 instances by a name you assign them (via tags).
+
+How does it work?
+
+* Add a tag called `awsdns` to the EC2 instances you want to give a name. The value of this tag will become the DNS name of the instance.
+* Run `awsdns` as a local DNS server on your machines.
+* Look up any EC2 instance by using the DNS name you assigned.
+
+Possible uses:
+
+* **Configuring dependencies by name**: For example, your application might need a mail server, a file sever, and a database server. You can give those servers meaningful names like `mailserver.intranet.` instead of hard-coding IPs in your config files.
+* **Adapting to infrastructure changes**: On ephemeral machines, IP address change all the time. Usign a DNS name lets all machines that depend on it discover the new one, without reconfiguring anything. This helps you when you start/stop machines, replace them with different instance types or for auto-scaling groups.
+* **Load balancing**: If several instances have the same name, `awsdns` will resolve their names randomly, spreading the worload evenly between them.
+
+## Usage
+
+`awsdns` accepts the following configuration parameters:
+
+* `--bind=ipaddress:port`: binding address and port. Sets both TCP and UDP. By default, `127.0.0.1:53`. Please note that you will need to be root to be able to access port 53.
+* `--ttl=seconds`: time that the results will remain in cache, in seconds. This value is used for the TTL in the answers, so clients can cache them, and also internally for caching the responses given by AWS. By default, `30`.
+* `--tag=tagname`: tag name in EC2 that contains the DNS name. By default, `awsdns`.
+* `--alternateDNS=dns-server`: DNS server to resolve names not in the `awsdns.` domain.
+* `--verbose`: Chattier version
+
+### AWS Region
+
+Region is taken from, in this order:
+
+* `AWS_REGION` environment variable
+* `~/.aws/config` file, if no environment variable is configured
+* Region where the instance is running, if you're running in EC2 and no environment variable nor config file is configured
+
+You can configure the region in the `~/.aws/config` file, like this:
+
+```
+[d`fault]
+region = us-east-1
+```
+
+Region can also be configured with the `AWS_REGION` environment variable. For example `AWS_REGION=us-east-1`.
+
+### AWS Credentials
+
+Credentials are taken from, in this order:
+
+* `AWS_PROFILE` environment variable
+* `~/.aws/credentials` file, if no environment variable is configured
+* IAM profile associated to the instance, if you're running in EC2, if you're running in EC2 and no environment variable nor credentials file is configured
+
+You will need AWS credentials to describe instances. Credentials can be supplied in an 
+`~/.aws/credentials` file, which might look like:
+
+```
+[default]
+aws_access_key_id = AKID1234567890
+aws_secret_access_key = MY-SECRET-KEY
+```
+
+If you have several profiles, you can select the profile using the `AWS_PROFILE` environment variable.
+
+You can learn more about the credentials file from this
+[blog post](http://blogs.aws.amazon.com/security/post/Tx3D6U6WSFGOK2H/A-New-and-Standardized-Way-to-Manage-Credentials-in-the-AWS-SDKs).
+
+Alternatively, you can set the following environment variables:
+
+```
+AWS_ACCESS_KEY_ID=AKID1234567890
+AWS_SECRET_ACCESS_KEY=MY-SECRET-KEY
+```
+
+#### AWS Policy
+
+The credentials you provide need to have access to the describe-instances EC2 API. You will need a policy similar (or identical) to this one:
+
+```
+{
+    "Statement": [
+        {
+            "Action": [
+                "ec2:DescribeInstances"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+                "*"
+            ]
+        }
+    ],
+    "Version": "2012-10-17"
+}
+```
---
+++ main.go
@@ -0,0 +1,190 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"math/rand"
+	"net"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/miekg/dns"
+	cache "github.com/patrickmn/go-cache"
+)
+
+var (
+	bind         *string
+	ttl          *uint
+	tag          *string
+	kche         *cache.Cache
+	alternateDNS *string
+	verbose      *bool
+)
+
+func queryDNSnamesForEC2instances(tagName string, tagValue string) ([]string, error) {
+	// aws ec2 describe-instances --filters "Name=tag:Name,Values=xxx" "Name=instance-state-name,Values=running" --query 'Reservations[0].Instances[*].PublicDnsName'
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	svc := ec2.New(sess)
+
+	params := &ec2.DescribeInstancesInput{
+		Filters: []*ec2.Filter{
+			{Name: aws.String("tag:" + tagName), Values: []*string{aws.String(tagValue)}},
+			{Name: aws.String("instance-state-name"), Values: []*string{aws.String("running")}},
+		},
+	} //TODO could iterate using NextToken: to get more results, accepted as a limitation for now
+	resp, err := svc.DescribeInstances(params)
+	if err != nil {
+		return nil, err
+	}
+
+	// extract dns names
+	results := []string{}
+	for _, reservation := range resp.Reservations {
+		for _, instance := range reservation.Instances {
+			results = append(results, *instance.PublicDnsName)
+		}
+	}
+	return results, nil
+}
+
+func queryDNSnameOnEC2(name string) ([]string, uint32, error) {
+	var cnames []string
+	var cnameTTL uint32
+	// get from cache
+	val, exp, found := kche.GetWithExpiration(name)
+	if found { // use cached value
+		cnames = val.([]string)
+		cnameTTL = uint32(exp.Sub(time.Now()).Seconds())
+	} else { // query value in ec2
+		q, err := queryDNSnamesForEC2instances(*tag, name)
+		if err != nil {
+			return nil, 0, err
+		}
+		// store in cache
+		kche.Set(name, q, cache.DefaultExpiration)
+		cnames = q
+		cnameTTL = uint32(*ttl)
+	}
+
+	// randomize order
+	for i := range cnames {
+		j := rand.Intn(i + 1)
+		cnames[i], cnames[j] = cnames[j], cnames[i]
+	}
+	return cnames, cnameTTL, nil
+}
+
+func handleDNSRequest(w dns.ResponseWriter, r *dns.Msg) {
+	if r.Opcode == dns.OpcodeQuery && strings.HasSuffix(r.Question[0].Name, "awsdns.") {
+		handleAWSDNSRequest(w, r)
+	} else {
+		handleAlternateDNSRequest(w, r)
+	}
+}
+
+func handleAWSDNSRequest(w dns.ResponseWriter, r *dns.Msg) {
+	m := new(dns.Msg)
+	m.SetReply(r)
+
+	if r.Opcode == dns.OpcodeQuery {
+		for _, q := range r.Question {
+			ec2Name := strings.TrimSuffix(q.Name, ".awsdns.")
+			cnames, ttl, err := queryDNSnameOnEC2(ec2Name)
+			if err != nil {
+				log.Println("Error looking up name", ec2Name, err)
+				dns.HandleFailed(w, r)
+				return
+			} else {
+				if *verbose {
+					log.Println("AWS query result:", cnames)
+				}
+				for _, cname := range cnames {
+					rr := new(dns.CNAME)
+					rr.Hdr = dns.RR_Header{
+						Name:   q.Name,
+						Rrtype: dns.TypeCNAME,
+						Class:  dns.ClassINET,
+						Ttl:    ttl,
+					}
+					rr.Target = cname + "."
+					m.Answer = append(m.Answer, rr)
+					if *verbose {
+						log.Println("Appending answer", rr)
+					}
+				}
+			}
+		}
+	}
+
+	w.WriteMsg(m)
+}
+
+func handleAlternateDNSRequest(w dns.ResponseWriter, r *dns.Msg) {
+	if *verbose {
+		log.Println("Handling request on alternate server:", r)
+	}
+
+	transport := "udp"
+	if _, ok := w.RemoteAddr().(*net.TCPAddr); ok {
+		transport = "tcp"
+	}
+
+	c := &dns.Client{Net: transport}
+	resp, _, err := c.Exchange(r, *alternateDNS)
+	if err != nil {
+		dns.HandleFailed(w, r)
+		return
+	}
+	w.WriteMsg(resp)
+
+}
+
+func main() {
+	// Parse flags
+	bind = flag.String("bind", "127.0.0.1:53", "binding address and port (both tcp/udp)")
+	ttl = flag.Uint("ttl", 30, "time the the results will remain in cache, in seconds")
+	tag = flag.String("tag", "awsdns", "tag name to be matched by dns query")
+	alternateDNS = flag.String("alternate-dns", "169.254.169.253", "dns server where queries will be redirected if not in the awsdns. zone")
+	verbose = flag.Bool("verbose", false, "verbose logging")
+	flag.Parse()
+
+	// cache
+	expirationDuration := time.Duration(*ttl) * time.Second
+	cleanupInterval := time.Duration(10) * expirationDuration
+	kche = cache.New(expirationDuration, cleanupInterval)
+
+	// Attach request handler func
+	dns.HandleFunc(".", handleDNSRequest)
+
+	// Start server
+	udpServer := &dns.Server{Addr: *bind, Net: "udp"}
+	tcpServer := &dns.Server{Addr: *bind, Net: "tcp"}
+	dns.HandleFunc(".", handleDNSRequest)
+	go func() {
+		if err := udpServer.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	go func() {
+		if err := tcpServer.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// Wait for SIGINT or SIGTERM
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	<-sigs
+
+	udpServer.Shutdown()
+	tcpServer.Shutdown()
+}
